f3fe582e6e1e40e7d8610cba4a83ca87d8b25240
mohit.goyal.sp <mohit.goyal.sp@lhsystems.com>
2022-08-13 15:06:21 +0530

    corrected e2e test case

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('assignment app is running!');
+    expect(page.getTitleText()).toEqual('FalconSat');
   });
 
   afterEach(async () => {
